40712321d59a64189613a21a5eb34fb0161a7dca
JoAn <joannacalinska@gmail.com>
2018-10-18 09:00:02 +0200

    disabled button and list of users - first part

--- src/Form.js
+++ src/Form.js
@@ -2,29 +2,49 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
 
 const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 const ipRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
 
 class Form extends React.Component {
   state = {
-    email: '',
-    nickname: '',
-    ipAddress: '',
+    user: {
+      email: '',
+      nickname: '',
+      ipAddress: '',
+    },
     errorMsg: '',
     isErrorEmail: true,
     isErrorIp: true,
+    usersArray: [],
   };
 
   handleChange = name => event => {
-    this.setState({
+    const {user} = this.state;
+    const newUser = {
+      ...user,
       [name]: event.target.value,
-    });
-  };
-
-  handleChangeEmail = event => {
+    }
     this.setState({
+      user: newUser,
+    }, () => console.log(this.state.user.nickname));
+  };
+  
+  handleChangeEmail = event => {
+    const {user} = this.state;
+    const newUser = {
+      ...user,
       email: event.target.value,
+    }
+    this.setState({
+      user: newUser,
     });
     const validEmail = emailRegex.test(event.target.value);
     if (validEmail) {
@@ -35,8 +55,13 @@ class Form extends React.Component {
   };
 
   handleChangeIp = event => {
-    this.setState({
+    const {user} = this.state;
+    const newUser = {
+      ...user,
       ipAddress: event.target.value,
+    }
+    this.setState({
+      user: newUser,
     });
     const validIp = ipRegex.test(event.target.value);
     if (validIp) {
@@ -48,53 +73,77 @@ class Form extends React.Component {
 
   submitUser = event => {
     event.preventDefault();
-    console.log('submitting');    
+    console.log('submitting');
+    const { usersArray } = this.state;
+    const newUsersArray = [...usersArray, this.state.user];
+    this.setState({ usersArray: newUsersArray });
   };
 
   render() {
     const { classes } = this.props;
+    const isInvalid = false;
+    // const isInvalid = this.state.isErrorEmail || this.state.isErrorIp;
+    console.log(this.state.usersArray);
     return (
-      <Grid container direction="column">
-        <form noValidate autoComplete="off" onSubmit={this.submitUser}>
-          <Grid item>
-            <TextField
-              id="email"
-              label="Your email"
-              value={this.state.email}
-              onChange={this.handleChangeEmail}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              id="nickname"
-              label="Your nick name"
-              value={this.state.nickname}
-              onChange={this.handleChange('nickname')}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item>        
-            <TextField
-              id="ip"
-              label="Your IP Address"
-              value={this.state.ipAddress}
-              onChange={this.handleChangeIp}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item>
-            <Button 
-              variant="contained"
-              type="submit"
-              color="primary"
-              disabled={this.state.isErrorEmail && this.state.isErrorIp}
-            >
-              Create User
-            </Button>       
-          </Grid>
-        </form>
-      </Grid>
+      <React.Fragment>
+        <Grid container direction="column">
+          <form noValidate autoComplete="off" onSubmit={this.submitUser}>
+            <Grid item>
+              <TextField
+                id="email"
+                label="Your email"
+                value={this.state.email}
+                onChange={this.handleChangeEmail}
+                margin="normal"
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="nickname"
+                label="Your nick name"
+                value={this.state.nickname}
+                onChange={this.handleChange('nickname')}
+                margin="normal"
+              />
+            </Grid>
+            <Grid item>        
+              <TextField
+                id="ip"
+                label="Your IP Address"
+                value={this.state.ipAddress}
+                onChange={this.handleChangeIp}
+                margin="normal"
+              />
+            </Grid>
+            <Grid item>
+              <Button 
+                variant="contained"
+                type="submit"
+                color="primary"
+                disabled={isInvalid}
+              >
+                Add User
+              </Button>       
+            </Grid>
+          </form>
+        </Grid>
+        <Grid container>
+          <List component="nav">
+            <ListItem button>
+              <ListItemIcon>
+                <DeleteIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <DeleteIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItem>
+          </List>
+        </Grid>
+      </React.Fragment>
     );
   }
 }
